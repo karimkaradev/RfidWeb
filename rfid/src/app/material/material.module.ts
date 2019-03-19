8ece1d22c126e422d7477200c5ffe2a540cfd28d
t1t142 <thierryverrier@orange.fr>
2019-03-19 16:32:38 +0100

    initial

---
+++ rfid/src/app/material/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule, MatNativeDateModule,
+  MatIconModule, MatSidenavModule,
+  MatListModule, MatToolbarModule
+} from '@angular/material';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+        ],
+
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+})
+export class MaterialModule { }
