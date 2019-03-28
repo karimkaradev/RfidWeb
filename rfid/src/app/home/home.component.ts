5331b8a21e5632dc8d75bda8c0b19dba3fbb02b9
karim KARA <karim.kara0201@gmail.com>
2019-03-28 15:47:40 +0100

    optimisation html/css

--- rfid/src/app/home/home.component.ts
+++ rfid/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TypeCours } from '../models/TypeCours';
-import { fakeTypeCours } from 'src/assets/fake_typecours';
+import { IImage } from 'ng-simple-slideshow';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +7,36 @@ import { fakeTypeCours } from 'src/assets/fake_typecours';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-typeCours:TypeCours[];
-  constructor() { }
+
+  imageUrls: (string | IImage)[] = [
+    { url: '../../../assets/images/slider1.jpg', caption: 'des dizaines de sport proposés', href: '#config' },
+    { url: '../../../assets/images/slider2.jpg', clickAction: () => alert('custom click function') },
+    { url: '../../../assets/images/slider3.jpg', caption: 'Une salle ouverte en permanence', href: '#' },
+  ];
+  height: string = '500px';
+  minHeight: string;
+  arrowSize: string = '30px';
+  showArrows: boolean = true;
+  disableSwiping: boolean = false;
+  autoPlay: boolean = true;
+  autoPlayInterval: number = 9999;
+  stopAutoPlayOnSlide: boolean = true;
+  debug: boolean = false;
+  backgroundSize: string = 'cover';
+  backgroundPosition: string = 'center center';
+  backgroundRepeat: string = 'no-repeat';
+  showDots: boolean = true;
+  dotColor: string = '#FFF';
+  showCaptions: boolean = true;
+  captionColor: string = '#FFF';
+  captionBackground: string = 'rgba(0, 0, 0, .35)';
+  lazyLoad: boolean = false;
+  hideOnNoSlides: boolean = false;
+  width: string = '100%';
+  fullscreen: boolean = false;
 
   ngOnInit() {
-    this.typeCours=fakeTypeCours;
+    
+   
   }
-more(q){
-
-}
 }
